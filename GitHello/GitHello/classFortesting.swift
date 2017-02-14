c1208fc5407ec061ca71b664eab330efb5acebc5
Abdullah <Abdullah@VIFTECHs-MacBook-Pro.local>
2017-02-14 12:16:54 +0500

    added new class for testing

---
+++ GitHello/GitHello/classFortesting.swift
@@ -0,0 +1,17 @@
+//
+//  classFortesting.swift
+//  GitHello
+//
+//  Created by Abdullah on 14/02/2017.
+//  Copyright © 2017 abdullah. All rights reserved.
+//
+
+import UIKit
+
+class classFortesting: UIViewController {
+    
+    func getGitRepo () {
+        // Working on git
+    }
+
+}
